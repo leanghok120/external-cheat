99c68a903807749aaed815f61acf97165329153c
leanghok <leanghokoeng5@gmail.com>
2025-05-18 18:45:58 +0700

    add user input

--- hack.c
+++ hack.c
@@ -6,11 +6,19 @@ int main() {
   int int_read = 0;
   int *ptr2int_read;
   int ptr2int_read2;
-  // dont forget to change the addr and pid to the ones u get in the dummy program
-  lm_address_t var_int_addr = 0x7fff916b4d60;
-  lm_address_t ptr2int_addr = 0x7fff916b4d68;
-  lm_pid_t pid = 15391;
+  lm_address_t var_int_addr;
+  lm_address_t ptr2int_addr;
+  lm_pid_t pid;
   lm_process_t proc;
+
+  printf("enter the pid: ");
+  scanf("%d", &pid);
+  printf("enter the varint address: ");
+  scanf("%lx", &var_int_addr);
+  printf("enter the ptr2int address: ");
+  scanf("%lx", &ptr2int_addr);
+  // remove newline char after scanf
+  getchar();
 
   if(LM_GetProcessEx(pid, &proc) != LM_TRUE) {
     fprintf(stderr, "failed to attach to pid: %d\n", pid); 
@@ -22,7 +30,7 @@ int main() {
     fprintf(stderr, "failed to read address: 0x%lx\n", var_int_addr); 
     return 1;
   }
-  printf("int_read = %d\n", int_read);
+  printf("\nint_read = %d\n", int_read);
 
   // ptr2int
   if(LM_ReadMemoryEx(&proc, ptr2int_addr, (lm_byte_t*)&ptr2int_read, sizeof(ptr2int_addr)) == 0) {
@@ -35,7 +43,7 @@ int main() {
   }
   printf("ptr2int = %d\n", ptr2int_read2);
 
-  printf("press enter to quit...\n");
+  printf("\npress enter to quit...\n");
   getchar();
 
   return 0;
